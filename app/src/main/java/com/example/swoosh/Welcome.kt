8f080114bd04e525210476200b09d752df8d1ff6
Jiang Pei <18917944949@189.cn>
2019-01-30 10:18:02 +0800

    first commit

---
+++ app/src/main/java/com/example/swoosh/Welcome.kt
@@ -0,0 +1,12 @@
+package com.example.swoosh
+
+import android.app.Activity
+import android.os.Bundle
+
+class Welcome : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
